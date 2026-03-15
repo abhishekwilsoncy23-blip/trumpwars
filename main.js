6cfd745971aafe752b8c21e63bb4a76ab9231e50
abhishekwilsoncy23-blip <abhishekwilson.cy23@jecc.ac.in>
2026-03-15 13:20:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,100 +1,102 @@
-// Global Game Variables accessible by all levels
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const uiLayer = document.getElementById("uiLayer");
 const uiMessage = document.getElementById("uiMessage");
-const btnRestart = document.getElementById("btnRestart");
-const btnNextLevel = document.getElementById("btnNextLevel");
+const uiButton = document.getElementById("uiButton");
+const mainMenu = document.getElementById("mainMenu");
 
-// Global Assets
-const playerImg = new Image(); playerImg.src = 't.png';
-const satImg = new Image(); satImg.src = 'satellite.png'; 
+let currentLevel = null;
+let animationId;
+let keys = {};
 
-// Input State
-const keys = {};
+// Load your generic player image here (make sure t.png is in your folder)
+let playerImg = new Image();
+playerImg.src = "t.png"; 
+
+// Keyboard Listeners
 window.addEventListener("keydown", (e) => keys[e.code] = true);
 window.addEventListener("keyup", (e) => keys[e.code] = false);
 
-function bindTouch(elementId, keyCode) {
-    const el = document.getElementById(elementId);
-    el.addEventListener("touchstart", (e) => { e.preventDefault(); keys[keyCode] = true; });
-    el.addEventListener("touchend", (e) => { e.preventDefault(); keys[keyCode] = false; });
+// Touch Listeners for Mobile Buttons
+function setupTouch(id, keyStr) {
+    const btn = document.getElementById(id);
+    btn.addEventListener("touchstart", (e) => { e.preventDefault(); keys[keyStr] = true; });
+    btn.addEventListener("touchend", (e) => { e.preventDefault(); keys[keyStr] = false; });
+    btn.addEventListener("mousedown", (e) => { e.preventDefault(); keys[keyStr] = true; });
+    btn.addEventListener("mouseup", (e) => { e.preventDefault(); keys[keyStr] = false; });
 }
-bindTouch("btnLeft", "ArrowLeft");
-bindTouch("btnRight", "ArrowRight");
-const btnAction = document.getElementById("btnAction");
-btnAction.addEventListener("touchstart", (e) => { e.preventDefault(); keys["Space"] = true; keys["ArrowUp"] = true; });
-btnAction.addEventListener("touchend", (e) => { e.preventDefault(); keys["Space"] = false; keys["ArrowUp"] = false; });
+setupTouch("btnLeft", "ArrowLeft");
+setupTouch("btnRight", "ArrowRight");
+setupTouch("btnAction", "Space");
 
-// Level Manager (State Machine)
-let currentLevel = null;
-let isPlaying = false;
-
-function changeLevel(newLevel) {
-    currentLevel = newLevel;
-    currentLevel.init();
-}
-
-// Global UI Helpers
-function triggerGameOver(reason, retryCallback) {
-    isPlaying = false;
-    uiLayer.style.display = "flex"; 
-    uiMessage.style.color = "#e74c3c";
-    uiMessage.innerHTML = "GAME OVER<br><span style='font-size: 0.5em; color: white'>" + reason + "</span>";
-    btnRestart.style.display = "block"; 
-    btnNextLevel.style.display = "none";
-    btnRestart.onclick = () => {
-        isPlaying = true;
-        retryCallback();
-    };
-}
-
-function triggerVictory(message, nextLevelObject) {
-    isPlaying = false;
-    uiLayer.style.display = "flex"; 
-    uiMessage.style.color = "#2ecc71";
-    uiMessage.innerHTML = message;
-    btnRestart.style.display = "none";
-    
-    if (nextLevelObject) {
-        btnNextLevel.style.display = "block";
-        btnNextLevel.innerText = "Start Next Level";
-        btnNextLevel.onclick = () => {
-            isPlaying = true;
-            changeLevel(nextLevelObject);
-        };
-    } else {
-        btnNextLevel.style.display = "none";
-    }
-}
-
+// Core Game Functions
 function checkCollision(rect1, rect2) {
-    return rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x &&
-           rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y;
+    return (rect1.x < rect2.x + rect2.width &&
+            rect1.x + rect1.width > rect2.x &&
+            rect1.y < rect2.y + rect2.height &&
+            rect1.y + rect1.height > rect2.y);
 }
 
-// Main Game Loop
 function gameLoop() {
-    if (isPlaying && currentLevel) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (currentLevel) {
         currentLevel.update();
         currentLevel.draw(ctx);
     }
-    requestAnimationFrame(gameLoop);
+    animationId = requestAnimationFrame(gameLoop);
 }
 
-// Start Screen Logic
-const startScreen = document.getElementById("startScreen");
-startScreen.addEventListener("click", () => {
-    const docElm = document.documentElement;
-    if (docElm.requestFullscreen) docElm.requestFullscreen();
-    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
-    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+function changeLevel(newLevel) {
+    cancelAnimationFrame(animationId);
+    uiLayer.style.display = "none";
+    mainMenu.style.display = "none";
+    currentLevel = newLevel;
+    if (currentLevel) {
+        currentLevel.init();
+        gameLoop();
+    }
+}
+
+function triggerGameOver(msg, retryCallback) {
+    cancelAnimationFrame(animationId);
+    currentLevel = null;
+    uiLayer.style.display = "flex";
+    uiMessage.innerHTML = `<span style="color:red; font-weight:bold;">GAME OVER</span><br><br>${msg}`;
+    uiButton.innerText = "TRY AGAIN";
+    uiButton.onclick = retryCallback;
+}
+
+function triggerVictory(msg, nextLevel) {
+    cancelAnimationFrame(animationId);
+    currentLevel = null;
+    uiLayer.style.display = "flex";
+    uiMessage.innerHTML = `<span style="color:lime; font-weight:bold;">SUCCESS!</span><br><br>${msg}`;
     
-    startScreen.style.display = "none";
-    isPlaying = true;
-    
-    // Start Level 1! (Make sure Level1 is loaded via HTML script tag)
-    changeLevel(Level1); 
-    gameLoop();
-});
+    if (nextLevel) {
+        uiButton.innerText = "NEXT LEVEL";
+        uiButton.onclick = () => changeLevel(nextLevel);
+    } else {
+        uiButton.innerText = "MAIN MENU";
+        uiButton.onclick = () => { uiLayer.style.display = "none"; mainMenu.style.display = "flex"; };
+    }
+}
+
+// Save & Load System
+function loadSavedGame() {
+    let savedLevel = parseInt(localStorage.getItem("myGame_savedLevel")) || 1;
+    if (savedLevel === 3) changeLevel(Level3);
+    else if (savedLevel === 2) changeLevel(Level2);
+    else changeLevel(Level1);
+}
+
+function startNewGame() {
+    localStorage.removeItem("myGame_savedLevel");
+    changeLevel(Level1);
+}
+
+// Check if there's a save to show the Continue button
+window.onload = () => {
+    if (!localStorage.getItem("myGame_savedLevel")) {
+        document.getElementById("continueBtn").style.display = "none";
+    }
+};
